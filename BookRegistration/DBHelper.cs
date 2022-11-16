e4ea84c1db6dffb0e5dc5e5aace06fad19e8da6d
Michael Forbes <michael.e.forbes@comcast.net>
2022-11-16 15:17:30 -0800

    Added helper function to get database connection

--- BookRegistration/DBHelper.cs
+++ BookRegistration/DBHelper.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace BookRegistration
 {
     static class DBHelper
     {
-        //GetConnection
+        /// <summary>
+        /// Gets the SqlConnection to the desired database
+        /// </summary>
+        /// <returns>The SqlConnection to the datbase</returns>
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection("Data Source=DESKTOP-N31PS9J;Initial Catalog=BookRegistration;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
     }
 }
